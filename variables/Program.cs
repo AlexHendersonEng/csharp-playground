04785155236c29651d60d00dfb0ecfe1ba9bf0ad
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-20 22:58:40 +0000

    Added variables project

---
+++ variables/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and then assign variable
+            int x;
+            x = 10;
+            System.Console.WriteLine("Variable declared then assigned: " + x);
+
+            // Declare and assign variable
+            int y = 20;
+            System.Console.WriteLine("Variable declared and assigned: " + y);
+
+            // Initialise multiple variables on one line
+            int a = 1, b = 2, c = 3;
+            System.Console.WriteLine("Multiple variable initialisation: a = " + a + ", b = " + b + ", c = " + c);
+        }
+    }
+}
