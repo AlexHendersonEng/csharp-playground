40aa4090187c136958dcf22a1a91b3280923c114
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-20 22:54:52 +0000

    Added data_types project

---
+++ data_types/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Integer types
+            int myInt = 15;
+            Console.WriteLine("int: " + myInt);
+            long myLong = 100L;
+            Console.WriteLine("long: " + myLong);
+
+            // Floating point types
+            float myFloat = 5.75F;
+            Console.WriteLine("float: " + myFloat);
+            double myDouble = 9.99D;
+            Console.WriteLine("double: " + myDouble);
+
+            // Booleans
+            bool myBool = false;
+            Console.WriteLine("bool: " + myBool);
+
+            // Characters
+            char myChar = 'a';
+            Console.WriteLine("char: " + myChar);
+
+            // Strings
+            string myString = "Hello World!";
+            Console.WriteLine("string: " + myString);
+        }
+    }
+}
