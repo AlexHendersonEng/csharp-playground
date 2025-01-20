63a28139d038933fe172ec17380466c49071f62e
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-20 23:05:10 +0000

    Added constants project

---
+++ constants/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+    const: A keyword used in front of the variable type which declares a variable
+           as constant meaning it is unchangeable and read only.
+*/ 
+
+namespace constants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define constant variable
+            const int constVar = 10;
+            System.Console.WriteLine("Constant variable: " + constVar);
+        }
+    }
+}
