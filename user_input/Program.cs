5a68df34ea6a2fa884ba0b1a5f9c670e41b6dfcc
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-21 22:06:00 +0000

    Added user_input project

---
+++ user_input/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace user_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask user for name
+            Console.WriteLine("Enter name:");
+            string name = Console.ReadLine();
+
+            // Print message to user using user input
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
