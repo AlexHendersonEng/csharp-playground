e1113529044d686afb6673889906f80cfd828cc7
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-22 21:54:02 +0000

    Added constructors project

---
+++ constructors/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+    constructor: A method named the same as the class name that is called on object instantiation.
+*/
+
+namespace constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate Person object (uses constructor)
+            Person person = new Person("Bob");
+
+            // Print Person member
+            Console.WriteLine($"Person name: {person.name}");
+
+            // Call Person method
+            person.sayHello();
+        }
+
+        // Person class
+        class Person
+        {
+            // member
+            public string name;
+
+            // constructor
+            public Person(string name)
+            {
+                this.name = name;
+            }
+
+            // method
+            public void sayHello()
+            {
+                Console.WriteLine($"{name} says hello");
+            }
+        }
+    }
+}
