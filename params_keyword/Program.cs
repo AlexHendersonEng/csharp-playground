62b4141395328f97fc840d1f208cc8a9a816472a
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-22 20:32:00 +0000

    Added params_keyword project

---
+++ params_keyword/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace params_keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call sum_elements method
+            double res = sum_elements(1.1, 2.2, 3.3, 4.4, 5.5);
+            Console.WriteLine(res);
+        }
+
+        static double sum_elements(params double[] a)
+        {
+            return a.Sum();
+        }
+    }
+}
