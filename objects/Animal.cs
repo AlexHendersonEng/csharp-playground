7399c758301e4a2bf55247e7e77f1e04bbff4d21
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-22 21:46:13 +0000

    Added objects project

---
+++ objects/Animal.cs
@@ -0,0 +1,15 @@
+﻿namespace objects
+{
+    // Animal class
+    internal class Animal
+    {
+        // member
+        public bool alive = true;
+
+        // method
+        public void run()
+        {
+            Console.WriteLine("The animal runs");
+        }
+    }
+}
---
+++ objects/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate Person object
+            Person person = new Person();
+            person.name = "Bob";
+
+            // Print Person member
+            Console.WriteLine($"Person name: {person.name}");
+
+            // Call Person method
+            person.sayHello();
+
+            // Instantiate Animal object
+            Animal animal = new Animal();
+
+            // Print Animal member
+            Console.WriteLine($"Animal alive: {animal.alive}");
+
+            // Call Animal method
+            animal.run();
+        }
+    }
+
+    // Person class
+    class Person
+    {
+        // member
+        public string name;
+
+        // method
+        public void sayHello()
+        {
+            Console.WriteLine($"{name} says hello");
+        }
+    }
+}
