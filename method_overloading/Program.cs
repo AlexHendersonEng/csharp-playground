3b22cc1ad6e6a18405c7412ab905387fc1b21e87
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-22 19:33:28 +0000

    Added method_overloading project

---
+++ method_overloading/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+    method overloading: Allows methods to have the same name but with different parameters and return types.
+*/
+
+namespace method_overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call add method with integers
+            int res1 = add(1, 2);
+            Console.WriteLine("Overloaded add method for integers: " + res1);
+
+            // Call add method with doubles
+            double res2 = add(1.5, 2.6);
+            Console.WriteLine("Overloaded add method for doubles: " + res2);
+        }
+
+        static int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static double add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
