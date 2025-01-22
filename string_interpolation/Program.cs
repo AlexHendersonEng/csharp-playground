a07453f927fa7c728dd5914ae9bdb4a53541e549
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-22 19:01:02 +0000

    Added string_interpolation project

---
+++ string_interpolation/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+    String interpolation: Allows for the substitution of variables into placeholders in a string.  
+*/
+
+namespace string_interpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "First";
+            string lastName = "Last";
+            string name = $"My full name is: {firstName} {lastName}";
+            Console.WriteLine(name);
+        }
+    }
+}
