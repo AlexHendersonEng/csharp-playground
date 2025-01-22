e36dba7a40ae13755224a94cede621878d1e6e0d
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-22 20:51:32 +0000

    Added exception_handling project

---
+++ exception_handling/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace exception_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try // Execute block of code that might throw an exception
+            {
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception e) // Execute code if exception occurs
+            { 
+                Console.WriteLine("Exeception has occured");
+                Console.WriteLine(e.ToString());
+            }
+            finally // Execute code whether exception occurs or not
+            {
+                Console.WriteLine("This is always executed!");
+            }
+        }
+    }
+}
