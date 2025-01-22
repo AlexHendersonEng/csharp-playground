7cdfb2d1a6bbf9797da2c1875881bd09e15cbc4b
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-22 20:58:47 +0000

    Added ternary_operator project

---
+++ ternary_operator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ternary_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ternary operator
+            bool condition = true;
+            string res = (condition) ? "The condition was true" : "The condition was false";
+            Console.WriteLine(res);
+        }
+    }
+}
