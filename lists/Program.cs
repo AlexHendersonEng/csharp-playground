7751699564e7922753b4e1dffd65c886131ba91b
Alex Henderson <alexhendersnao@gmail.com>
2025-01-25 19:46:20 +0000

    Added lists project

---
+++ lists/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+    lists: A data structure that represents a list of objects that can be accessed
+           by an index. It is similar to an array but can dynamically increase/decrease
+           in size.
+*/
+
+// Import List definition which sits in 'System.Collections.Generic'
+using System.Collections.Generic;
+
+namespace lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define list
+            List<string> list = new List<string>();
+
+            // Add strings to list
+            list.Add("a");
+            list.Add("b");
+            list.Add("c");
+
+            // Print out strings
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
