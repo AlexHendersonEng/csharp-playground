64e020d3425355db3662cc839fcbd539148cc5b0
Alex Henderson <alexhendersnao@gmail.com>
2025-01-25 20:35:45 +0000

    Added lambda_expressions project

---
+++ lambda_expressions/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+    lambda expressions: Allows for the creation of an anonymous function using the
+                        lambda declaration operator '=>'.
+*/
+
+namespace lambda_expressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare lambda function
+            Func<int, int> squaredNumbers = x => x * x;
+
+            // Use and print out result of lambda function
+            Console.WriteLine(squaredNumbers(4));
+        }
+    }
+}
