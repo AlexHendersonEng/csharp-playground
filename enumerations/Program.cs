f4f4d4d00edfd4a081b0e0d4e428966043f6e07a
Alex Henderson <alexhendersnao@gmail.com>
2025-01-25 23:32:11 +0000

    Added enumerations project

---
+++ enumerations/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+    enums: A special class that contains a set of named integer constants.
+*/
+
+namespace enumerations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print enumeration information
+            Console.WriteLine(Weekdays.Monday + " is a weekday");
+            Console.WriteLine(Weekdays.Tuesday + " has value " + (int)Weekdays.Tuesday);
+        }
+    }
+
+    // Define enumeration (default numbering starts at 0 but can be explicitly defined)
+    enum Weekdays
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5
+    }
+}
