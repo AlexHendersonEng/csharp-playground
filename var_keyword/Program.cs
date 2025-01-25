2a1b7fa90707b37cb0828b195977e48f52a2db67
Alex Henderson <alexhendersnao@gmail.com>
2025-01-25 19:30:05 +0000

    Added var_keyword project

---
+++ var_keyword/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+    var: Used to declare an implicit type variable.
+*/
+
+namespace var_keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define variables using var keyword
+            var a = 1;
+            var b = 2.0;
+            var c = "Hello";
+
+            // Print out values
+            Console.WriteLine("Variable a: " + a + ", Type: " + a.GetType());
+            Console.WriteLine("Variable b: " + b + ", Type: " + b.GetType());
+            Console.WriteLine("Variable c: " + c + ", Type: " + c.GetType());
+        }
+    }
+}
